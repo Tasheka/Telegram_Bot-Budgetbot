49dfdf2e7353f4e48707d7077cee31face3e912b
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-02-01 23:02:04 -0500

    Flesh out income

--- bot.rb
+++ bot.rb
@@ -20,56 +20,67 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: "What would you like to do next? \n /Budget \n /Tips")
       
     when '/Budget'
-      bot.api.send_message(chat_id: message.chat.id, text: "Select 'Income' to enter your incomes earned. \n /Expenses")
+      bot.api.send_message(chat_id: message.chat.id, text: "Select 'Income' to enter your incomes earned. \n /Income")
 
     when '/Income'
-        bot.api.send_message(chat_id: message.chat.id, text: "Enter your basic salary amount.")
+        bot.api.send_message(chat_id: message.chat.id, text: "Select the expense you want to enter: \n /PrimaryIncome \n /AdditionalIncome")
+
+    when '/PrimaryIncome'
+        bot.api.send_message(chat_id: message.chat.id, text: "Enter your primary income.")
+        bot.api.send_message(chat_id: message.chat.id, text: "Select 'Back to Incomes' to view the list of incomes. \n /BackToIncomes")
+
+    when '/AdditionalIncome'
+        bot.api.send_message(chat_id: message.chat.id, text: "Enter your additional income.")
+        bot.api.send_message(chat_id: message.chat.id, text: "Select 'Back to Incomes' to view the list of incomes. \n /BackToIncomes")
+
+    when '/BackToIncomes'
+        bot.api.send_message(chat_id: message.chat.id, text: "Select the income you want to enter: \n /PrimaryIncome \n /AdditionalIncome")
+        bot.api.send_message(chat_id: message.chat.id, text: "Select 'Total Income' when you are finished entering your incomes: \n /TotalIncome")
+    
+    when '/TotalIncome'
+        tip = RandomTipsTricks.new
+        bot.api.send_message(chat_id: message.chat.id, text: "Your total income is $#{tip.total_income}")
+        bot.api.send_message(chat_id: message.chat.id, text: "Select 'Expenses' to enter your expenses. \n /Expenses")
 
    else
      $income.push(message.text.to_i)
-     bot.api.send_message(chat_id: message.chat.id, text: "Enter any additional income earned.")
-     bot.api.send_message(chat_id: message.chat.id, text: "Select 'Expenses' when you have no more incomes to enter. \n /Expenses")
-
-    end
-
 
     case message.text
     when '/Expenses'
         tip = RandomTipsTricks.new
-        bot.api.send_message(chat_id: message.chat.id, text: "Your total income is $#{tip.total_income}")
         bot.api.send_message(chat_id: message.chat.id, text: "Select the expense you want to enter: \n /Rent \n /Food \n /Transportation \n /Bills \n /AdditionalExpenses")
-    
     when '/Rent'
-        bot.api.send_message(chat_id: message.chat.id, text: "Enter the amount you pay for this expense.")
+        bot.api.send_message(chat_id: message.chat.id, text: "Enter the amount you pay for rent.")
         bot.api.send_message(chat_id: message.chat.id, text: "Select 'Back to Expenses' to view the list of expenses. \n /BackToExpenses")
 
     when '/Food'
-        bot.api.send_message(chat_id: message.chat.id, text: "Enter the amount you pay for this expense.")
+        bot.api.send_message(chat_id: message.chat.id, text: "Enter the amount you pay for food.")
         bot.api.send_message(chat_id: message.chat.id, text: "Select 'Back to Expenses' to view the list of expenses. \n /BackToExpenses")
 
     when '/Transportation'
-        bot.api.send_message(chat_id: message.chat.id, text: "Enter the amount you pay for this expense.")
+        bot.api.send_message(chat_id: message.chat.id, text: "Enter the amount you pay for transportation.")
         bot.api.send_message(chat_id: message.chat.id, text: "Select 'Back to Expenses' to view the list of expenses. \n /BackToExpenses")
 
     when '/Bills'
-        bot.api.send_message(chat_id: message.chat.id, text: "Enter the amount you pay for this expense.")
+        bot.api.send_message(chat_id: message.chat.id, text: "Enter the amount you pay for bills")
         bot.api.send_message(chat_id: message.chat.id, text: "Select 'Back to Expenses' to view the list of expenses. \n /BackToExpenses")
 
     when '/AdditionalExpenses'
-        bot.api.send_message(chat_id: message.chat.id, text: "Select the expense you want to enter: \n /Rent \n /Food \n /Transportation \n /Bills \n /AdditionalExpenses")
+        bot.api.send_message(chat_id: message.chat.id, text: "Enter the amount you pay for additional expenses")
         bot.api.send_message(chat_id: message.chat.id, text: "Select 'Back to Expenses' to view the list of expenses. \n /BackToExpenses")
 
     when '/BackToExpenses'
         bot.api.send_message(chat_id: message.chat.id, text: "Select the expense you want to enter: \n /Rent \n /Food \n /Transportation \n /Bills \n /AdditionalExpenses")
-        bot.api.send_message(chat_id: message.chat.id, text: "Select 'Calculate' when you are finished entering your expenses: \n /Calculate")
+        bot.api.send_message(chat_id: message.chat.id, text: "Select 'Total Expenses' when you are finished entering your expenses: \n /TotalExpenses")
     
-    when '/Calculate'
+    when '/TotalExpenses'
         tip = RandomTipsTricks.new
         bot.api.send_message(chat_id: message.chat.id, text: "Your total expense is $#{tip.total_expense}")
     else
         $expense.push(message.text.to_i)
     end
+    end
+    end
   end
-end
 
 # rubocop:enable Layout/LineLength
