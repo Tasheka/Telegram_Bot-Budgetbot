e5d1b2c77dcb2c05f3956fff907491749af5b28f
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-02-02 12:35:01 -0500

    Correct  errors

--- bin/bot.rb
+++ bin/bot.rb
@@ -6,8 +6,6 @@ require 'telegram/bot'
 require_relative '../lib/tips_tricks_list'
 require_relative '../lib/income_expenses_calculations'
 
-token = 
-
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
