b1f9e89d0c321e859371c2877cf70b73dc08d0a9
Tasheka Hamilton <tasheka.tech@gmail.com>
2021-02-03 20:45:15 -0500

    Add additional tests

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -2,10 +2,15 @@ require_relative '../lib/tips_tricks_list'
 require_relative '../lib/income_expenses_calculations'
 
 describe RandomTipsTricks do
-  describe '#random_tips_tricks' do
+  describe '#output_tip' do
     it 'gives a random quote from an array of quotes' do
       random_tips = RandomTipsTricks.new
-      expect(random_tips.random_tips_tricks.class).to eq(String)
+      expect(random_tips.output_tip.class).to eq(String)
+    end
+
+    it 'gives does not return a number' do
+        random_tips = RandomTipsTricks.new
+        expect(random_tips.output_tip.class).not_to eq(Integer)
     end
   end
 end
@@ -16,6 +21,11 @@ describe Calculations do
       cal = Calculations.new
       expect(cal.total_income.class).to eq(Integer)
     end
+
+    it 'calculates annd outputs the total income entered by a user' do
+        cal = Calculations.new
+        expect(cal.total_income.class).not_to eq(String)
+    end
   end
 
   describe '#total_expense' do
@@ -23,12 +33,22 @@ describe Calculations do
       cal = Calculations.new
       expect(cal.total_expense.class).to eq(Integer)
     end
+
+    it 'calculates annd outputs the total expenses entered by a user' do
+        cal = Calculations.new
+        expect(cal.total_expense.class).not_to eq(String)
+    end
   end
 
   describe '#left_over' do
     it 'calculates annd outputs the difference between the total income and total expenes entered by a user' do
       cal = Calculations.new
       expect(cal.left_over.class).to eq(Integer)
+    end
+
+    it 'calculates annd outputs the difference between the total income and total expenes entered by a user' do
+        cal = Calculations.new
+        expect(cal.left_over.class).not_to eq(String)
     end
   end
 end
